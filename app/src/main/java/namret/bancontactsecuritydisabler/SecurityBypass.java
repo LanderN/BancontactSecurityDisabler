7bfdd9dcaec1287f43111e9bcf443d22cdf0cca4
Lander Noterman <lander.noterman@ugent.be>
2015-10-22 10:03:07 +0200

    Initial commit

---
+++ app/src/main/java/namret/bancontactsecuritydisabler/SecurityBypass.java
@@ -0,0 +1,44 @@
+package namret.bancontactsecuritydisabler;
+
+import android.content.Context;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+/**
+ * This module bypasses the security in the Bancontact app
+ * Created by namret on 21/10/2015 (Great Scott!).
+ */
+public class SecurityBypass implements IXposedHookLoadPackage {
+
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable
+    {
+        if (
+                lpparam.packageName.equals("mobi.inthepocket.bcmc.bancontact")
+                )
+        {
+            XposedBridge.log("Loaded " + lpparam.packageName + ", hooking root methods");
+
+            try
+            {
+                findAndHookMethod("o.v", lpparam.classLoader, "ʼ", "o.ﾒ", XC_MethodReplacement.returnConstant(0));
+                findAndHookMethod("o.v", lpparam.classLoader, "ʻ", "o.ﾒ", XC_MethodReplacement.returnConstant(false));
+                findAndHookMethod("o.v", lpparam.classLoader, "ˊ", String.class, "o.ﾒ", XC_MethodReplacement.returnConstant(null));
+
+                findAndHookMethod("ᴵ", lpparam.classLoader, "ˊ", XC_MethodReplacement.returnConstant(false));
+                findAndHookMethod("ᴵ", lpparam.classLoader, "ˋ", Context.class,  XC_MethodReplacement.returnConstant(false));
+                findAndHookMethod("ᴵ", lpparam.classLoader, "ˋ", XC_MethodReplacement.returnConstant(false));
+
+            }
+            catch (NoSuchMethodError e)
+            {
+                XposedBridge.log("Method not found " + e.getMessage());
+            }
+        }
+    }
+}
+
